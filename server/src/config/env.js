7e72a143e97b2450f19711f56ae1d04a91448ff7
Tamuonanthit <hkhiyen1@gmail.com>
2026-09-10 20:55:28 +0700

    chore: setup shared resource

---
+++ server/src/config/env.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    APP_PORT: process.env.APP_PORT,
+    APP_BASE_URL: process.env.APP_BASE_URL,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_TOKEN_COOKIE_EXPIRES: process.env.JWT_TOKEN_COOKIE_EXPIRES,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    JWT_REFRESH_TOKEN_SECRET_KEY: process.env.JWT_REFRESH_TOKEN_SECRET_KEY,
+    JWT_ACCESS_TOKEN_EXPIRES: process.env.JWT_ACCESS_TOKEN_EXPIRES,
+    JWT_REFRESH_TOKEN_EXPIRES: process.env.JWT_REFRESH_TOKEN_EXPIRES,
+};
