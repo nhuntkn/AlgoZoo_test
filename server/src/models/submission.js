7e4d780bb81885e83225796cde4f1adff6230994
Jash Nguyen <jashlynguyen@gmail.com>
2026-09-12 00:14:45 +0700

    Add contentBlockSchema

--- server/src/models/submission.js
+++ server/src/models/submission.js
@@ -1,3 +1,33 @@
+
+const contentBlockSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      enum: ["text", "code", "image", "file"],
+      required: true,
+    },
+
+    content: {
+      type: String,
+    },
+
+    language: {
+      type: String,
+    },
+
+    file_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "File",
+    },
+
+    filename: {
+      type: String,
+    },
+  },
+  { _id: false }
+);
+
+
 const SubmissionSchema = new mongoose.Schema(
   {
     class_problem_id: {
@@ -10,13 +40,10 @@ const SubmissionSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
-    content_blocks: [
-      {
-        text: { type: String, required: true },
-        image: { type: String, required: true },
-        code: { type: String, required: true },
-      },
-    ],
+    content_blocks: {
+        type: [contentBlockSchema],
+        required: true,
+    },
     is_late: { type: Boolean, default: false },
     status: {
       type: String,
@@ -30,3 +57,4 @@ const SubmissionSchema = new mongoose.Schema(
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
 );
+
