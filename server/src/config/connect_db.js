e3b73c1ad72be835746bfd33a18a2322a0e214ad
Jash Nguyen <jashlynguyen@gmail.com>
2026-09-12 00:01:42 +0700

    fix: restore MongoDB DNS resolution and server configuration

--- server/src/config/connect_db.js
+++ server/src/config/connect_db.js
@@ -1,7 +1,9 @@
 const { MONGO_URI } = require("../config/env");
 const mongoose = require("mongoose");
+const dns = require("dns");
 
 const connectionString = MONGO_URI;
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 mongoose.set('strictQuery', false);
 
 const connectDatabase = async () => {
